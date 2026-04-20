77893a987f645e4dcc3ebf31b736bdebc5e2270a
pawaan <pawaan@pawaans-MacBook-Air.local>
2026-04-20 16:02:15 +0530

    test dealexample

--- test/KiratCoin.t.sol
+++ test/KiratCoin.t.sol
@@ -7,20 +7,20 @@ import "src/KiratCoin.sol";
 contract TestKiratCoin is Test {
     KiratCoin c; 
 
-event Transfer(address indexed from, address indexed to, uint256 value);
 
 
     function setUp() public {
          c = new KiratCoin();
     }
 
-function testTransferEmit() public {
-    c.mint(address(this), 100);
-    vm.expectEmit(true, true, false, false);
+function test_DealExample() public {
+   address account = 0x1483783755C6d08070B69Dd9e3391c7C00e5F23c;
 
-    emit Transfer(address(this),0x1483783755C6d08070B69Dd9e3391c7C00e5F23c, 100);
+   //set the balance of 'account' to 'ether '
+   vm.deal(account, 10 ether);
 
-    c.transfer(0x1483783755C6d08070B69Dd9e3391c7C00e5F23c, 10);
+   //assert that the balance is set correctly 
+   assertEq(address(account).balance,  10 ether);
 
 }
 
